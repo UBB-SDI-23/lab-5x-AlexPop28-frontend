2b940ff0f603801482f02e88dd69685e5ea97de4
Alex Pop <popalexnicolae@gmail.com>
2023-04-19 14:11:23 +0300

    Add GenericTable and refactor AllMovies

---
+++ src/components/GenericTable.tsx
@@ -0,0 +1,84 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useState } from "react";
+
+interface Column {
+  headElement?: JSX.Element;
+  bodyElement: (value: any, index: number) => JSX.Element;
+  sortKey?: string;
+}
+
+interface GenericTableProps {
+  data: any[];
+  columns: Column[];
+  noDataElement?: JSX.Element;
+}
+
+export const GenericTable: React.FC<GenericTableProps> = ({
+  data,
+  columns,
+  noDataElement,
+}) => {
+  const [sort, setSort] = useState<{ column: string; order: "asc" | "desc" }>({
+    column: "",
+    order: "asc",
+  });
+  const [sortedData, setSortedData] = useState(data);
+
+  const sortData = (column: string) => {
+    const isAsc = sort.column === column && sort.order === "asc";
+    setSort({ column, order: isAsc ? "desc" : "asc" });
+    setSortedData(
+      [...data].sort((a: any, b: any) => {
+        if (isAsc) {
+          return a[column] < b[column] ? -1 : 1;
+        } else {
+          return a[column] < b[column] ? 1 : -1;
+        }
+      })
+    );
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {columns.map((column, index) => (
+              <TableCell
+                key={index}
+                onClick={() => column.sortKey && sortData(column.sortKey)}
+              >
+                {column.headElement}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sortedData.length === 0 && (
+            <TableRow>
+              <TableCell colSpan={columns.length}>{noDataElement}</TableCell>
+            </TableRow>
+          )}
+          {sortedData.length > 0 &&
+            sortedData.map((value, rowIndex) => (
+              <TableRow key={value.id}>
+                {columns.map((column, colIndex) => (
+                  <TableCell key={colIndex}>
+                    {column.bodyElement(value, rowIndex)}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
--- src/pages/AllMovies.tsx
+++ src/pages/AllMovies.tsx
@@ -6,19 +6,13 @@ import {
   CircularProgress,
   Container,
   IconButton,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   TextField,
   Tooltip,
   Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { GenericTable } from "../components/GenericTable";
 import { Pager } from "../components/Pager";
 import useAxios from "../lib/hooks/useAxios";
 import { Movie } from "../models/movie";
@@ -31,16 +25,80 @@ const createMovieUrl = (page: number, pageSize: number, minRating: number) => {
   return `/movies?${searchParams.toString()}`;
 };
 
+const getColumns = (page: number, pageSize: number) => {
+  return [
+    {
+      headElement: <>#</>,
+      bodyElement: (_: any, index: number) => (
+        <>{pageSize * (page - 1) + index + 1}</>
+      ),
+    },
+    {
+      headElement: <>Name</>,
+      bodyElement: (movie: Movie, _: any) => (
+        <Link to={`/movies/${movie.id}/details`} title="View movie details">
+          {movie.name}
+        </Link>
+      ),
+      sortKey: "name",
+    },
+    {
+      headElement: <>Rating</>,
+      bodyElement: (movie: Movie, _: any) => <>{movie.rating}</>,
+      sortKey: "rating",
+    },
+    {
+      headElement: <>Release year</>,
+      bodyElement: (movie: Movie, _: any) => (
+        <>{movie.release_date.substring(0, 4)}</>
+      ),
+      sortKey: "release_date",
+    },
+    {
+      headElement: <>Length (minutes)</>,
+      bodyElement: (movie: Movie, _: any) => <>{movie.length_in_minutes}</>,
+      sortKey: "length_in_minutes",
+    },
+    {
+      bodyElement: (movie: Movie, _: any) => (
+        <>
+          <IconButton
+            component={Link}
+            sx={{ mr: 3 }}
+            to={`/movies/${movie.id}/details`}
+          >
+            <Tooltip title="View movie details" arrow>
+              <ReadMoreIcon color="primary" />
+            </Tooltip>
+          </IconButton>
+
+          <IconButton
+            component={Link}
+            sx={{ mr: 3 }}
+            to={`/movies/${movie.id}/edit`}
+          >
+            <EditIcon />
+          </IconButton>
+
+          <IconButton
+            component={Link}
+            sx={{ mr: 3 }}
+            to={`/movies/${movie.id}/delete`}
+          >
+            <DeleteForeverIcon sx={{ color: "red" }} />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
+};
+
 export const AllMovies = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [movies, setMovies] = useState<Movie[]>([]);
   const [pageSize, setPageSize] = useState(10);
   const [count, setCount] = useState(0);
-  const [sort, setSort] = useState<{ column: string; order: "asc" | "desc" }>({
-    column: "",
-    order: "asc",
-  });
   const [searchParams] = useSearchParams();
   const [page, setPage] = useState(Number(searchParams.get("page")) || 1);
   const [minRating, setMinRating] = useState(
@@ -55,33 +113,18 @@ export const AllMovies = () => {
     setMovies(results);
     setCount(count);
     setLoading(false);
-  };
-
-  const loadPage = () => {
-    fetchMovies();
     navigate(createMovieUrl(page, pageSize, minRating), { replace: true });
   };
 
   useEffect(() => {
-    loadPage();
+    fetchMovies();
   }, [page, pageSize]);
 
-  const sortData = (column: string) => {
-    const isAsc = sort.column === column && sort.order === "asc";
-    setSort({ column, order: isAsc ? "desc" : "asc" });
-    const sortedMovies = [...movies].sort((a: Movie, b: Movie) => {
-      if (isAsc) {
-        return a[column] < b[column] ? -1 : 1;
-      } else {
-        return a[column] < b[column] ? 1 : -1;
-      }
-    });
-    setMovies(sortedMovies);
-  };
+  const columns = getColumns(page, pageSize);
 
   return (
     <Container>
-      <h1>All movies</h1>
+      <Typography variant="h3">All movies</Typography>
       <TextField
         label="Minimum rating"
         placeholder={"0"}
@@ -89,7 +132,7 @@ export const AllMovies = () => {
       />
       <Button
         onClick={() => {
-          page === 1 ? loadPage() : setPage(1);
+          page === 1 ? fetchMovies() : setPage(1);
         }}
       >
         Filter
@@ -104,90 +147,15 @@ export const AllMovies = () => {
       <br />
 
       {loading && <CircularProgress />}
-      {!loading && movies.length === 0 && (
-        <Typography variant="h4">No movies found</Typography>
-      )}
-      {!loading && movies.length > 0 && (
+      {!loading && (
         <>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left">#</TableCell>
-                  <TableCell align="left" onClick={() => sortData("name")}>
-                    Name
-                  </TableCell>
-                  <TableCell align="right" onClick={() => sortData("rating")}>
-                    Rating
-                  </TableCell>
-                  <TableCell
-                    align="right"
-                    onClick={() => sortData("release_date")}
-                  >
-                    Release year
-                  </TableCell>
-                  <TableCell
-                    align="right"
-                    onClick={() => sortData("length_in_minutes")}
-                  >
-                    Length (minutes)
-                  </TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {movies.map((movie, index) => (
-                  <TableRow key={movie.id}>
-                    <TableCell scope="row">
-                      {pageSize * (page - 1) + index + 1}
-                    </TableCell>
-                    <TableCell scope="row">
-                      <Link
-                        to={`/movies/${movie.id}/details`}
-                        title="View movie details"
-                      >
-                        {movie.name}
-                      </Link>
-                    </TableCell>
-                    <TableCell align="right">{movie.rating}</TableCell>
-                    <TableCell align="right">
-                      {movie.release_date.substring(0, 4)}
-                    </TableCell>
-                    <TableCell align="right">
-                      {movie.length_in_minutes}
-                    </TableCell>
-                    <TableCell align="right">
-                      <IconButton
-                        component={Link}
-                        sx={{ mr: 3 }}
-                        to={`/movies/${movie.id}/details`}
-                      >
-                        <Tooltip title="View movie details" arrow>
-                          <ReadMoreIcon color="primary" />
-                        </Tooltip>
-                      </IconButton>
-
-                      <IconButton
-                        component={Link}
-                        sx={{ mr: 3 }}
-                        to={`/movies/${movie.id}/edit`}
-                      >
-                        <EditIcon />
-                      </IconButton>
-
-                      <IconButton
-                        component={Link}
-                        sx={{ mr: 3 }}
-                        to={`/movies/${movie.id}/delete`}
-                      >
-                        <DeleteForeverIcon sx={{ color: "red" }} />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <GenericTable
+            data={movies}
+            columns={columns}
+            noDataElement={
+              <Typography variant="h4">No movies found</Typography>
+            }
+          />
           <Pager
             page={page}
             setPage={setPage}
