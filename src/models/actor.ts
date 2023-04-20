9152e86c1df8f164bc1cb3a80b57509309db3dd7
Alex Pop <popalexnicolae@gmail.com>
2023-04-20 11:52:36 +0300

    Add Actor model

---
+++ src/models/actor.ts
@@ -0,0 +1,11 @@
+import { Movie } from "./movie";
+
+export interface Actor {
+  id: number;
+  name: string;
+  alternative_name?: string;
+  date_of_birth: string;
+  birthplace: string;
+  height_in_cm: number;
+  movies: Movie[];
+}
