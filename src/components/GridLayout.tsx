d9cbb08ef24587804152a233c3e43d8c6555ea98
Alex Pop <popalexnicolae@gmail.com>
2023-04-22 10:55:23 +0300

    Update children prop of GridLayout

--- src/components/GridLayout.tsx
+++ src/components/GridLayout.tsx
@@ -1,14 +1,15 @@
 import { Grid } from "@mui/material";
+import React, { ReactNode } from "react";
 
 interface GridLayoutProps {
-  children: JSX.Element[];
+  children: ReactNode;
 }
 
 export const GridLayout: React.FC<GridLayoutProps> = ({ children }) => {
   return (
     <Grid container spacing={2}>
       {children &&
-        children.map((child, index) => (
+        React.Children.map(children, (child, index) => (
           <Grid item xs={12} key={index}>
             {child}
           </Grid>
