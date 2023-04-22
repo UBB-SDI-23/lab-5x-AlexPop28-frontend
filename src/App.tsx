42316f0c4b9c46c777948c802fbd994c5943e62d
Alex Pop <popalexnicolae@gmail.com>
2023-04-22 12:03:38 +0300

    Add ActorEdit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "./components/Navbar";
 import "./constants";
 import { AppHome } from "./pages/AppHome";
 import { ActorCreate } from "./pages/actors/ActorCreate";
+import { ActorEdit } from "./pages/actors/ActorEdit";
 import { AllActors } from "./pages/actors/AllActors";
 import { AllMovies } from "./pages/movies/AllMovies";
 import { MovieCreate } from "./pages/movies/MovieCreate";
@@ -24,6 +25,7 @@ export default () => {
           <Route path="/movies/:movieId/edit" element={<MovieEdit />}></Route>
           <Route path="/actors" element={<AllActors />} />
           <Route path="/actors/add" element={<ActorCreate />} />
+          <Route path="/actors/:actorId/edit" element={<ActorEdit />}></Route>
         </Routes>
       </BrowserRouter>
     </ConfirmProvider>
---
+++ src/pages/actors/ActorEdit.tsx
@@ -0,0 +1,104 @@
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import LockIcon from "@mui/icons-material/Lock";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import {
+  Card,
+  CardContent,
+  CircularProgress,
+  Container,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { DeleteButton } from "../../components/DeleteButton";
+import { ActorForm } from "../../components/custom/ActorForm";
+import useAxios from "../../lib/hooks/useAxios";
+import { Actor } from "../../models/actor";
+
+export const ActorEdit = () => {
+  const { actorId } = useParams();
+  const [actor, setActor] = useState<Actor>({
+    name: "",
+    alternative_name: "",
+    date_of_birth: "",
+    birthplace: "",
+    height_in_cm: 0,
+  });
+  const [loading, setLoading] = useState(true);
+  const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
+  const axios = useAxios();
+  const BASE_URL = `/actors/${actorId}/`;
+
+  const fetchActor = async () => {
+    setLoading(true);
+    const { data: fetchedActor } = await axios.get(BASE_URL);
+    setActor(fetchedActor);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchActor();
+  }, []);
+
+  const onSaveChanges = async () => {
+    try {
+      const { data } = await axios.put(BASE_URL, actor);
+      setDisabled(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const controlButtons = (
+    <>
+      {disabled && (
+        <IconButton sx={{ mr: 3 }} onClick={() => setDisabled(false)}>
+          <Tooltip title="Make changes" arrow>
+            <LockIcon />
+          </Tooltip>
+        </IconButton>
+      )}
+      {!disabled && (
+        <>
+          <IconButton sx={{ mr: 3 }} onClick={onSaveChanges}>
+            <Tooltip title="Save changes" arrow>
+              <LockOpenIcon />
+            </Tooltip>
+          </IconButton>
+          <DeleteButton
+            onDelete={async () => {
+              await axios.delete(`/actors/${actorId}/`);
+              navigate("/actors");
+            }}
+          />
+        </>
+      )}
+    </>
+  );
+
+  return (
+    <Container>
+      <Card>
+        <CardContent>
+          <Typography variant="h1">
+            {disabled ? "About the" : "Edit"} actor
+          </Typography>
+          {disabled && (
+            <IconButton component={Link} sx={{ mr: 3 }} to={`/actors`}>
+              <ArrowBackIcon />
+            </IconButton>
+          )}
+          {loading && <CircularProgress />}
+          {!loading && actor && (
+            <ActorForm actor={actor} setActor={setActor} disabled={disabled}>
+              {controlButtons}
+            </ActorForm>
+          )}
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
