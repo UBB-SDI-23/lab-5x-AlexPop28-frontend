2755452fd684d2c3ce11ce5f13436f375d48c853
Alex Pop <popalexnicolae@gmail.com>
2023-04-26 01:12:33 +0300

    Add page size select to pagination

--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, InputLabel, MenuItem, Select } from "@mui/material";
 
 interface PagerProps {
   page: number;
@@ -26,6 +26,19 @@ export const Pager = ({
       >
         Next
       </Button>
+      <InputLabel id="page-size-label" style={{ width: "auto" }}>
+        Page size
+      </InputLabel>
+      <Select
+        labelId="page-size-label"
+        label="Page size"
+        value={pageSize}
+        onChange={(event) => setPageSize(Number(event.target.value))}
+      >
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={25}>25</MenuItem>
+        <MenuItem value={100}>100</MenuItem>
+      </Select>
     </>
   );
 };
