3b7d0cfc735e635f36fb19e44bc9009d1a06e128
Alex Pop <popalexnicolae@gmail.com>
2023-05-10 10:56:43 +0300

    Add role edit for admins

--- src/pages/users/UserDetails.tsx
+++ src/pages/users/UserDetails.tsx
@@ -11,6 +11,7 @@ import { CardContainer } from "../../components/CardContainer";
 import { GridLayout } from "../../components/GridLayout";
 import useAxios from "../../lib/hooks/useAxios";
 import { UserProfile } from "../../models/UserProfile";
+import { isAdmin } from "../../utils/permissions";
 
 export const UserDetails = () => {
   const { username } = useParams();
@@ -21,20 +22,23 @@ export const UserDetails = () => {
     gender: "",
     birthday: "",
     marital_status: "",
+    role: "",
   });
   const user = localStorage.getItem("user");
   const [pageSize, setPageSize] = useState(() => {
     if (user) return JSON.parse(user).pageSize ?? 10;
     return 10;
   });
+  const [role, setRole] = useState("");
   const axios = useAxios();
 
-  const BASE_URL = `/users/${username}`;
+  const BASE_URL = `/users/${username}/`;
 
   const fetchUserProfile = async () => {
     setLoading(true);
     const { data } = await axios.get(BASE_URL);
     setUserProfile(data);
+    setRole(data.role);
     setLoading(false);
   };
 
@@ -53,6 +57,26 @@ export const UserDetails = () => {
           <p>Movies added: {userProfile.movie_count}</p>
           <p>Actors added: {userProfile.actor_count}</p>
           <p>Directors added: {userProfile.director_count}</p>
+          {user && isAdmin() && (
+            <FormControl fullWidth>
+              <InputLabel id="role-label">Role</InputLabel>
+              <Select
+                labelId="role-label"
+                label="Role"
+                value={role}
+                onChange={async (event) => {
+                  event.preventDefault();
+                  const newRole = event.target.value;
+                  setRole(newRole);
+                  await axios.patch(BASE_URL, { role: newRole });
+                }}
+              >
+                <MenuItem value="regular">Regular</MenuItem>
+                <MenuItem value="moderator">Moderator</MenuItem>
+                <MenuItem value="admin">Admin</MenuItem>
+              </Select>
+            </FormControl>
+          )}
           {user && (
             <FormControl fullWidth>
               <InputLabel id="page-size-label">Page size</InputLabel>
